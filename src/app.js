fcb350b84d9c7636ba4791bbcfe9bba07118484f
Trevor Alt <trevorjalt@gmail.com>
2020-09-17 10:40:38 -0400

    cleaning up code

--- src/app.js
+++ src/app.js
@@ -1,33 +1,33 @@
-require('dotenv').config()
-const express = require('express')
-const morgan = require('morgan')
-const cors = require('cors')
-const helmet = require('helmet')
-const { NODE_ENV } = require('./config')
+require('dotenv').config();
+const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const helmet = require('helmet');
+const { NODE_ENV } = require('./config');
 
-const app = express()
+const app = express();
 
 const morganOption = (NODE_ENV === 'production')
   ? 'dev'
   : 'common';
 
-app.use(morgan(morganOption))
-app.use(helmet())
-app.use(cors())
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
 
 app.get('/', (req, res) => {
-    res.send('Hello, world!')
+    res.send('Hello, world!');
 })
 
 app.use(function errorHandler(error, req, res, next) {
-    let response
+    let response;
     if (NODE_ENV === 'production') {
-        response = { error: { messages: 'server error' } }
+        response = { error: { messages: 'server error' } };
     } else {
         console.error(error)
-        response = { message: error.message, error }
+        response = { message: error.message, error };
     }
-    res.status(500).json(response)
+    res.status(500).json(response);
 })
 
 module.exports = app
